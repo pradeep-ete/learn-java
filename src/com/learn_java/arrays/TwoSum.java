881626627e497b28819504121b1a7c77882e662b
Pradeep Ete <Pradeep.Ete+ghub@blueyonder.com>
2026-07-04 12:28:05 +0530

    update names

--- src/com/learn_java/arrays/TwoSum.java
+++ src/com/learn_java/arrays/TwoSum.java
@@ -7,14 +7,14 @@ public class TwoSum {
 	public static void main(String[] args) {
 		int[] arr = { 2, 7, 11, 15, 3, 13 };
 		int target = 10;
-		Map<Integer,Integer>  freq = new HashMap<>();
+		Map<Integer,Integer>  indexMap = new HashMap<>();
 		int i=0;
 		for(int ele:arr){
-			if (freq.containsKey(target-ele)) {
-				System.out.println("found at index" + freq.get(target-ele) + "," + i);
+			if (indexMap.containsKey(target-ele)) {
+				System.out.println("found at index" + indexMap.get(target-ele) + "," + i);
 				return;
 			} else {
-				freq.put(ele,i);
+				indexMap.put(ele,i);
 			}
 			i++;
 		}
